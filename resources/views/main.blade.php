1557fd809667ee399a18ae2b738c895f06a65bd7
Gerard <gerardma1995@hotmail.com>
2017-07-05 17:50:29 +0200

    Starts project

---
+++ resources/views/main.blade.php
@@ -0,0 +1,231 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        {!! Html::style('css/main.css') !!}
+        <script src="https://use.fontawesome.com/5879b99bb3.js"></script>
+    </head>
+    <body>
+      <!-- Wrapper -->
+  			<div id="wrapper">
+
+  				<!-- Main -->
+  					<div id="main">
+  						<div class="inner">
+
+  							<!-- Header -->
+  								<header id="header">
+  									<a href="index.html" class="logo"><strong>Inicio</strong> Tallers Amoròs</a>
+  									<ul class="icons">
+  										<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+  										<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+  										<li><a href="#" class="icon fa-snapchat-ghost"><span class="label">Snapchat</span></a></li>
+  										<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+  										<li><a href="#" class="icon fa-medium"><span class="label">Medium</span></a></li>
+  									</ul>
+  								</header>
+
+  							<!-- Banner -->
+  								<section id="banner">
+  									<div class="content">
+  										<header>
+  											<h1>Talleres Amoros </h1>
+  											<p>Soluciones agrícolas</p>
+  										</header>
+  										<p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. Pellentesque sapien ac quam. Lorem ipsum dolor sit nullam.</p>
+  										<ul class="actions">
+  											<li><a href="#" class="button big">Learn More</a></li>
+  										</ul>
+  									</div>
+  									<span class="image object">
+  										<img src="images/pic10.jpg" alt="" />
+  									</span>
+  								</section>
+
+  							<!-- Section -->
+  								<section>
+  									<header class="major">
+  										<h2>Erat lacinia</h2>
+  									</header>
+  									<div class="features">
+  										<article>
+  											<span class="icon fa-diamond"></span>
+  											<div class="content">
+  												<h3>Portitor ullamcorper</h3>
+  												<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+  											</div>
+  										</article>
+  										<article>
+  											<span class="icon fa-paper-plane"></span>
+  											<div class="content">
+  												<h3>Sapien veroeros</h3>
+  												<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+  											</div>
+  										</article>
+  										<article>
+  											<span class="icon fa-rocket"></span>
+  											<div class="content">
+  												<h3>Quam lorem ipsum</h3>
+  												<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+  											</div>
+  										</article>
+  										<article>
+  											<span class="icon fa-signal"></span>
+  											<div class="content">
+  												<h3>Sed magna finibus</h3>
+  												<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+  											</div>
+  										</article>
+  									</div>
+  								</section>
+
+  							<!-- Section -->
+  								<section>
+  									<header class="major">
+  										<h2>Productos destacados</h2>
+  									</header>
+  									<div class="posts">
+  										<article>
+  											<a href="#" class="image"><img src="images/main/kit-hhte-38-be-hbp-20-ah-hbc-210-we.jpg" alt="" /></a>
+  											<h3>KIT HHTE 38 BE + HBP 20 AH + HBC 210 WE</h3>
+  											<p>La desbrozadora de batería Honda HHTE38BE es ideal para el mantenimiento de espacios con vegetación densa, hierba alta y maleza persistente (batería y cargador incluidos).</p>
+  											<ul class="actions">
+  												<li><a href="#" class="button">Más</a></li>
+  											</ul>
+  										</article>
+  										<article>
+  											<a href="#" class="image"><img src="images/main/hf-2315-sb.jpg" alt="" /></a>
+  											<h3>HF 2315 SB</h3>
+  											<p>Cortacésped de asiento Honda HF2315SB, transmisión mecánica, deflector y mulching opcional, Optiflow, comodidad y seguridad.</p>
+  											<ul class="actions">
+  												<li><a href="#" class="button">Más</a></li>
+  											</ul>
+  										</article>
+  										<article>
+  											<a href="#" class="image"><img src="images/main/deposito-rapidon-6-lts.jpg" alt="" /></a>
+  											<h3>Depósito RAPIDON 6 lts</h3>
+  											<p>Depósito transparente Rapidon6 con indicador de contenido, para repostar de la manera más cómoda, rápida y limpia, evitando derramar combustible.</p>
+  											<ul class="actions">
+  												<li><a href="#" class="button">Más</a></li>
+  											</ul>
+  										</article>
+  										<article>
+  											<a href="#" class="image"><img src="images/main/hhb-25.jpg" alt="" /></a>
+  											<h3>HHB 25</h3>
+  											<p>Utilizar el soplador Honda HHB 25, con sólo 4.5 Kg y motor de 4 tiempos, es la manera más cómoda y eficaz de mantener limpio tu jardín.</p>
+  											<ul class="actions">
+  												<li><a href="#" class="button">Más</a></li>
+  											</ul>
+  										</article>
+  										<article>
+  											<a href="#" class="image"><img src="images/main/hre-330.jpg" alt="" /></a>
+  											<h3>HRE 330</h3>
+  											<p>Cortacésped eléctrico Honda HRE330, económico, fácil y ligero, para jardines pequeños. Con portacables, cuchilla inteligente y protección contra sobre calentamiento.</p>
+  											<ul class="actions">
+  												<li><a href="#" class="button">Más</a></li>
+  											</ul>
+  										</article>
+  										<article>
+  											<a href="#" class="image"><img src="images/main/kit-hhbe-81-be-hbp-40-ah-hbc-210-we.jpg" alt="" /></a>
+  											<h3>KIT HHBE 81 BE + HBP 40 AH + HBC 210 WE</h3>
+  											<p>El soplador de batería Honda HHBE81BE permite limpiar sin esfuerzo la hojarasca y suciedad acumuladas, incluso espacios interiores (batería y cargador incluidos).</p>
+  											<ul class="actions">
+  												<li><a href="#" class="button">Más</a></li>
+  											</ul>
+  										</article>
+  									</div>
+  								</section>
+
+  						</div>
+  					</div>
+
+  				<!-- Sidebar -->
+  					<div id="sidebar">
+  						<div class="inner">
+
+  							<!-- Search -->
+  								<section id="search" class="alt">
+  									<form method="post" action="#">
+  										<input type="text" name="query" id="query" placeholder="Search" />
+  									</form>
+  								</section>
+
+  							<!-- Menu -->
+  								<nav id="menu">
+  									<header class="major">
+  										<h2>Menu</h2>
+  									</header>
+  									<ul>
+  										<li><a href="index.html">Inicio</a></li>
+  										<li>
+  											<span class="opener">Maquinaria</span>
+  											<ul>
+                          <li><a href="#">Batería de litio</a></li>
+      										<li><a href="#">Gasolina</a></li>
+  											</ul>
+  										</li>
+  										<li><a href="#">Contacto</a></li>
+  									</ul>
+  								</nav>
+
+  							<!-- Section -->
+  								<section>
+  									<header class="major">
+  										<h2>Novedades</h2>
+  									</header>
+  									<div class="mini-posts">
+  										<article>
+  											<a href="#" class="image"><img src="{!! asset('images/main/hrx-537-vye.jpg') !!}" alt="" /></a>
+  											<p>Cortacésped <b>Honda HRX 537 VYE</b>, estárter automático, jardines medios o grandes, sistema de corte Versamow® y avance automático Smart Drive®.</p>
+  										</article>
+  										<article>
+  											<a href="#" class="image"><img src="images/main/miimo-hrm-310.jpg" alt="" /></a>
+  											<p>Si lo que quieres es un césped perfecto sin preocuparte de nada, el <b>Honda Miimo 310</b> es la solución ideal. Para jardines de cualquier medida, hasta un máximo de 2.000 m2. Instalación no incluida en el precio.</p>
+  										</article>
+  									</div>
+  									<ul class="actions">
+  										<li><a href="#" class="button">Ver más</a></li>
+  									</ul>
+  								</section>
+
+  							<!-- Section -->
+  								<section>
+  									<header class="major">
+  										<h2>Contacta</h2>
+  									</header>
+  									<p>¿Tienes dudas acerca de uno de nuestros producots?</p>
+                    <p>¡Contacta con nosotros!</p>
+  									<ul class="contact">
+  										<li class="fa-envelope-o"><a href="#">talleresamoros@untitled.tld</a></li>
+  										<li class="fa-phone">(000) 000-0000</li>
+  										<li class="fa-home">1234 Somewhere Road #8254<br />
+  										Nashville, TN 00000-0000</li>
+  									</ul>
+  								</section>
+
+  							<!-- Footer -->
+  								<footer id="footer">
+  									<p class="copyright">&copy; Untitled. All rights reserved. Demo Images: <a href="https://unsplash.com">Unsplash</a>. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
+  								</footer>
+
+  						</div>
+  					</div>
+
+  			</div>
+
+  		<!-- Scripts -->
+      {!! Html::script('js/jquery.min.js') !!}
+      {!! Html::script('js/skel.min.js') !!}
+      {!! Html::script('js/util.js') !!}
+      {!! Html::script('js/main.js') !!}
+    </body>
+</html>
