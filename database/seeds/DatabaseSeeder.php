a7b10c38daa01c400a9465f9d0a4a85c014da36c
Gerard <gerardma1995@hotmail.com>
2017-09-28 18:49:59 +0200

    test

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,13 +15,13 @@ class DatabaseSeeder extends Seeder
         $this->call(PatentTableSeeder::class);
         $this->call(FeatureCategoryTableSeeder::class);
         $this->call(ItemTableSeeder::class);
-        $this->call(FeatureTablePesoSeeder::class);
+        // $this->call(FeatureTablePesoSeeder::class);
         $this->call(FeatureTableAnchCorteAndLongEspadaSeeder::class);
         $this->call(FeatureTableCilindradaSeeder::class);
         $this->call(FeatureTableAperturaCuchillaSeeder::class);
         $this->call(FeatureTableVelMotorSeeder::class);
         $this->call(FeatureTablePotenciaMaxSeeder::class);
         $this->call(FeatureTablePertigaAndLongitudSeeder::class);
-        $this->call(ItemFeatureTableSeeder::class);
+        // $this->call(ItemFeatureTableSeeder::class);
     }
 }
