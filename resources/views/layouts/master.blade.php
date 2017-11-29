b0eaab3fb2bc48f0dd2b2538b2520c10fa0482c3
Gerard <gerardma1995@hotmail.com>
2017-11-29 20:55:43 +0100

    Add google analytics

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -112,6 +112,15 @@
           {!! Html::script('js/skel.min.js') !!}
           {!! Html::script('js/util.min.js') !!}
           {!! Html::script('js/main.min.js') !!}
+          <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-110433092-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-110433092-1');
+        </script>
         @show
     </body>
 </html>
